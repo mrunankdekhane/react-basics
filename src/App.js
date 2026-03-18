9f3f3ba46e7694c3e32ddcbe48377f6a10ca4c4a
Mrunank Dekhane <mrunankdekhane@gmail.com>
2026-03-18 15:21:58 +0530

    main file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import Button from "./components/Button";
+
+function App(){
+  return (
+    <div>
+        <h1>My App component</h1>
+        <Button/>
+    </div> 
+  );
+  
+}
+export default App;
